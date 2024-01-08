e78cb935c5d3e8758f036a8995e29b2a7cf4961e
Zhuravlev Petr <zeterfrost@gmail.com>
2024-01-08 23:13:10 +0600

    added encode servise

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
 const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
     res.send('<p><b>Add any params for echo.</b></br> Example: http://localhost:8000/hello</p>')
-})
+});
+
+app.get('/encode/:text', (req, res) => {
+    const answer = Vigenere.Cipher('password').crypt(req.params.text);
+    res.send(answer);
+});
 
 app.get('/:message', (req, res) => {
     res.send(req.params.message);
@@ -12,4 +18,4 @@ app.get('/:message', (req, res) => {
 
 app.listen(port, () => {
     console.log('Server is started. Port: ' + port);
-})
+});
